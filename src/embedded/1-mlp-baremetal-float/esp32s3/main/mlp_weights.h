aee8c1004f00a88d618118a573047e78d221df32
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2023-01-16 16:36:39 -0300

    MLP weights

---
+++ src/embedded/1-mlp-baremetal-float/esp32s3/main/mlp_weights.h
@@ -0,0 +1,11 @@
+#ifndef MLP_WEIGHTS
+#define MLP_WEIGHTS
+
+#include <stdint.h>
+
+extern const float layer_0_weights[15840];
+extern const float layer_0_bias[96];
+extern const float layer_2_weights[1440];
+extern const float layer_2_bias[15];
+
+#endif // end of MLP_PARAMS
