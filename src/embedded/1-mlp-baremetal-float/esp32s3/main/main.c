1bbeaef7947ce85ba16d86f8f0a0787ac9e00f0e
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2023-01-25 16:00:41 -0300

    Finish float inference

--- src/embedded/1-mlp-baremetal-float/esp32s3/main/main.c
+++ src/embedded/1-mlp-baremetal-float/esp32s3/main/main.c
@@ -8,8 +8,14 @@
 #include <nnops.h>
 #include <mlp_weights.h>
 
-#define MY_UART UART_NUM_0
+#define UART_NUM UART_NUM_0
 
+#define CMD_NOOP 0
+#define CMD_INFERENCE_BEGIN 1
+
+// In a real-world implementation, we'd reuse
+// memory between layers, but for educational
+// purposes we're giving each a new buffer.
 float input[165];
 float layer_0_output[96];
 float layer_1_output[96];
@@ -25,9 +31,9 @@ uart_config_t uart_config = {
 };
 
 void setup_uart() {
-    ESP_ERROR_CHECK(uart_param_config(MY_UART, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(MY_UART, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    ESP_ERROR_CHECK(uart_driver_install(MY_UART, 1024 * 2, 1024 * 2, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, 1024 * 2, 1024 * 2, 0, NULL, 0));
 }
 
 void app_main(void)
@@ -35,42 +41,53 @@ void app_main(void)
     int r;
     char cmd = 0;
     int subject_id;
-    char msg_waiting[] = "Waiting for input\n";
-    char msg_reading[] = "Reading input\n";
-    char msg_processing[] = "Processing\n";
+    int begin, end, elapsed;
+
     char msg_ready[] = "Ready\n";
     char msg_error[] = "Error\n";
-    
+    char msg_waiting[] = "Waiting for input\n";
+
     setup_uart();
-    uart_write_bytes(MY_UART, msg_ready, strlen(msg_ready));
+    uart_write_bytes(UART_NUM, msg_ready, strlen(msg_ready));
 
     while (1) {
-        uart_write_bytes(MY_UART, msg_waiting, strlen(msg_waiting));
+        // log we are waiting for a new input
+        uart_write_bytes(UART_NUM, msg_waiting, strlen(msg_waiting));
 
-        while (cmd != 1)
-            uart_read_bytes(MY_UART, (char *)&cmd, sizeof(char), 100);
+        while (cmd != CMD_INFERENCE_BEGIN)
+            uart_read_bytes(UART_NUM, (char *)&cmd, sizeof(char), 100);
+        cmd = CMD_NOOP;
 
-        uart_write_bytes(MY_UART, msg_reading, strlen(msg_reading));
-
-        cmd = 0;
-        r = uart_read_bytes(MY_UART, (float *)input, 165*4, 100000);
+        // read a new input
+        r = uart_read_bytes(UART_NUM, (float *)input, 165*4, 100000);
         if (r != 165*sizeof(float))
-            uart_write_bytes(MY_UART, msg_error, strlen(msg_error));
-        
-        uart_write_bytes(MY_UART, msg_processing, strlen(msg_processing));
+            uart_write_bytes(UART_NUM, msg_error, strlen(msg_error));
 
-        // layer 0
+        // fetch current CPU cycle count
+        asm volatile("esync; rsr %0,ccount":"=a" (begin));
+
+        // neural net layer 0
         mvm(layer_0_weights, input, layer_0_output, 96, 165);
         add(layer_0_output, layer_0_bias, layer_0_output, 96);
 
-        // layer 1
+        // neural net layer 1
         relu(layer_0_output, layer_1_output, 96);
 
-        // layer 2
+        // neural net layer 2
         mvm(layer_2_weights, layer_1_output, layer_2_output, 15, 96);
         add(layer_2_output, layer_2_bias, layer_2_output, 15);
 
+        // neural net output
         subject_id = argmax(layer_2_output, 15);
-        uart_write_bytes(MY_UART, &subject_id, sizeof(int));
+
+        // fetch current CPU cycle count
+        asm volatile("esync; rsr %0,ccount":"=a" (end));
+
+        // log output
+        uart_write_bytes(UART_NUM, &subject_id, sizeof(int));
+
+        // log elapsed time
+        elapsed = end-begin;
+        uart_write_bytes(UART_NUM, (int*)&elapsed, sizeof(int));
     }
 }
